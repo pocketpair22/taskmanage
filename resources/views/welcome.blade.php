85a3dbb2eafc25bdf623be16e9ad31806b006390
vagrant <vagrant@localhost.localdomain>
2019-03-16 15:41:35 +0000

    edit welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laraveldesuyo
                 </div>
 
                 <div class="links">
