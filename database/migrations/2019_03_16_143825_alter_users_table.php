15f3e3fa539dee50ccdc5df783e7e415d12c81cf
vagrant <vagrant@localhost.localdomain>
2019-03-16 15:24:46 +0000

    migration setting

---
+++ database/migrations/2019_03_16_143825_alter_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('password', 255);
+            $table->string('email', 255)->unique();
+            $table->unsignedInteger('num_of_task')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
