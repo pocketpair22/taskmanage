7aa9ebd88d6594c2c269c1df90ebca46e39c3bab
pocketpair <toriwatashun@gmail.com>
2019-03-18 20:03:30 +0900

    welcome page edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laraveldesuyo
+                    Laravelyadena
                 </div>
 
                 <div class="links">
