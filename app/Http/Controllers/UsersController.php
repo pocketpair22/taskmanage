ec2b3e5f884f18b0b10308c7c7bd01e880d95b46
pocketpair <toriwatashun@gmail.com>
2019-03-19 16:51:33 +0900

    view.users add

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,11 +8,11 @@ class UsersController extends Controller
 {
     public function index() {
         die("exit");
-        $data = [
-            'msg'=>'indexdesu',
-        ];
+        //$data = [
+        //    'msg'=>'indexdesu',
+        //];
         
-        return view('users.index', $data);
+        //return view('users.index', $data);
     }
 
     public function add() {
