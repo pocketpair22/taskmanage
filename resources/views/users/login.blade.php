ee49573beacf44ba44b7af2a91eeab94526046df
pocketpair <toriwatashun@gmail.com>
2019-03-19 18:09:59 +0900

    login add

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -6,7 +6,7 @@
         <title>login</title>
     </head>
     <body>
-        <form action="login" method="post" accept-charset="utf-8">
+        <form action="users" method="post" accept-charset="utf-8">
             {!! csrf_field() !!}
             <p><label>ユーザ名：<input type="text" name="name" value="{{$name}}"></label></p>
             <p><label>パスワード：<input type="password" name="password" value=""></label></p>
